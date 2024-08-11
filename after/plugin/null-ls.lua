14995f586faa3c8544e2efd19a5dff81ccd9f306
Tiago Grosso <tiagogrosso99@gmail.com>
2024-08-11 22:37:32 +0100

    fix: reenable format on save, add eslint

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,10 +1,23 @@
 local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
 	sources = {},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format()
+				end,
+			})
+		end
+	end,
 })
 
 require("mason-null-ls").setup({
-	ensure_installed = { "stylua", "jq", "prettierd", "google-java-format" },
+	ensure_installed = { "stylua", "jq", "prettier", "google-java-format", "eslint_d" },
 	handlers = {},
 })
